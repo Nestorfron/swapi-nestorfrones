bd1ad6a278233365deaef89f4eaf21e3b69af4ad
nestorfron <nestorfrones07@hotmail.com>
2024-07-16 20:09:17 -0300

    starting

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Gender, Specie, Planet, Favorite
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +36,171 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#Get All Users:
 
-    return jsonify(response_body), 200
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    serialize_user = [user.serialize() for user in users]
+    return jsonify({
+        "users": serialize_user
+    }), 200
+
+#Get User by ID
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        user= User.query.get(user_id)
+        if user is None:
+            return  jsonify({"error": "user not found!"}),404
+        return jsonify({
+            "user": user.serialize()
+        }), 200
+    except Exception as error:
+        return jsonify({"error": f"{error}!"}),500
+
+#Create User:
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    body = request.json 
+    email = body.get("email", None)
+    password = body.get("password", None)
+    is_active = body.get("is_active", None)
+    if email is None or password is None or is_active is None:
+        return jsonify({"error": "missing fields"}), 400
+    user = User(email=email, password=password, is_active=is_active)
+    try:
+        db.session.add(user)
+        db.session.commit()
+        db.session.refresh(user)
+        return jsonify({"message": f"Character craterd {user.email}!"}),201
+    except Exception as error:
+        return jsonify({"error": f"{error}!"}),500
+
+#Get All People:
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people = Character.query.all()
+    serialize_character = [character.serialize() for character in people]
+    return jsonify({
+        "people": serialize_character
+    }), 200
+
+#Get People by ID:
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_character(people_id):
+    try:
+        character = Character.query.get(people_id)
+        if character is None:
+            return  jsonify({"error": "character not found!"}),404
+        return jsonify({
+            "character": character.serialize()
+        }), 200
+    except Exception as error:
+        return jsonify({"error": f"{error}!"}),500
+
+#Create character:
+
+@app.route('/people', methods=['POST'])
+def create_character():
+    body = request.json 
+    name = body.get("name", None)
+    gender = body.get("gender", None)
+    specie = body.get("specie", None)
+    if name is None or gender is None or specie is None:
+        return jsonify({"error": "missing fields"}), 400
+    character = Character(name=name, gender=Gender(gender), specie=Specie(specie))
+    try:
+        db.session.add(character)
+        db.session.commit()
+        db.session.refresh(character)
+        return jsonify({"message": f"Character craterd {character.name}!"}),201
+    except Exception as error:
+        return jsonify({"error": f"{error}!"}),500
+
+#Get All Planets
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets = Planet.query.all()
+    serialize_planet = [planet.serialize() for planet in planets]
+    return jsonify({
+        "planets": serialize_planet
+    }), 200
+
+#Get Planet by ID:
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    try:
+        planet = Planet.query.get(planet_id)
+        if planet is None:
+            return  jsonify({"error": "planet not found!"}),404
+        return jsonify({
+            "planet": planet.serialize()
+        }), 200
+    except Exception as error:
+        return jsonify({"error": f"{error}!"}),500
+
+#Create Planet:
+
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    body = request.json 
+    name = body.get("name", None)
+    diameter = body.get("diameter", None)
+    population = body.get("population", None)
+    if name is None or diameter is None or population is None:
+        return jsonify({"error": "missing fields"}), 400
+    planet = Planet(name=name, diameter=diameter, population=population)
+    try:
+        db.session.add(planet)
+        db.session.commit()
+        db.session.refresh(planet)
+        return jsonify({"message": f"Planet craterd {planet.name}!"}),201
+    except Exception as error:
+        return jsonify({"error": f"{error}!"}),500
+
+
+
+
+
+
+
+
+   
+
+
+
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,100 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum as PyEnum 
 
 db = SQLAlchemy()
+
+
+class Gender(PyEnum):
+    FEMALE = "Female"
+    MALE = "Male"
+    UNDEFINED = "Undefined"
+
+class Specie(PyEnum):
+    HUMAN = "Human"
+    ALIEN = "Alien"
+    ROBOT = "Robot"
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        f"<user{self.name}>"
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
+    specie = db.Column(db.Enum(Specie), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    planet = db.relationship('Planet', backref='characters')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "gender": self.gender.value,
+            "specie": self.specie.value
+        }
+
+    def __repr__(self):
+        f"<character{self.name}>"
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "diameter": self.diameter,
+            "population": self.population,
+        }
+
+    def __repr__(self):
+        f"<planet{self.name}>"
+
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def __repr__(self):
+        f"<favorite{self.name}>"
+    
+
+
+
+
+
+
+
+
+
+
+
